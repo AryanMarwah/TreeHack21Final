f0c84ccfcfe3ff2486cbc22eb8685ccf4bcb51a1
AryanMarwah <74761017+AryanMarwah@users.noreply.github.com>
2021-02-13 13:22:24 -0600

    Add files via upload

---
+++ FirebaseAuthWebAPP-master/js/landingPage.js
@@ -0,0 +1,12 @@
+// xxxxxxxxxx Redirect to the Login Page xxxxxxxxxx    
+function logIn()
+{
+
+        window.location.replace("./pages/sign-in.html");
+   
+}
+
+function createPost()
+{
+    window.location.replace("./pages/create-post.html");
+}
