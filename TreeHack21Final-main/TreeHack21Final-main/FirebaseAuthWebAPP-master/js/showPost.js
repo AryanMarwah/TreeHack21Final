a1ada4d17c368104873d22e4bf3e9a6bd9368d5e
Krishpandya682 <76144309+Krishpandya682@users.noreply.github.com>
2021-02-14 02:59:28 +0530

    Add files via upload

---
+++ TreeHack21Final-main/TreeHack21Final-main/FirebaseAuthWebAPP-master/js/showPost.js
@@ -0,0 +1,18 @@
+
+// xxxxxxxxxx Submitting and Creating new user in firebase authentication xxxxxxxxxx
+function showPost()
+{
+    var db = firebase.firestore();
+    const list_div =    document.querySelector("#list_div")
+    
+    db.collection("protestPosts").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+         list_div.innerHTML += "<div class = 'list-item'><br><h3>" + doc.data().protestTitle  + " </h3><p>"+doc.data().protestDescription+ "</p><p>"+doc.data().protestDate+ "</p><p>"+doc.data().protestTime+ "</p><p></p></div>"
+        });
+    });
+   
+
+}
+        
+   
+
